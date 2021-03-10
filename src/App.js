24ec4caffc4ea2d311ca2ee8dd4299dcc133f248
Siam <shahidulmd392@gmail.com>
2021-03-11 00:24:56 +0600

    try someting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 import Home from './components/Home/Home';
-// import { Images } from "./Images/Footbal-ground.jpg";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 import NotExisting from './components/NotExisting/NotExisting';
 import TeamInfo from './components/TeamInfo/TeamInfo';
